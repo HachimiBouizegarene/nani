af213167ea2d328495de663e2a60800183f7f7bb
Hachimi Bouizegarene <hachimi.bouizegarene@outlook.fr>
2025-09-03 16:24:58 +0200

    edit images srcs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,11 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 
 const images = [
-    'https://images.squarespace-cdn.com/content/v1/5a88864349fc2b8e071336f8/1741914254873-B8S1H8PU7QQJPU91JR2G/2025_portfolio_short1.jpg?format=1500w',
-    'https://cdn.discordapp.com/attachments/762066841920995378/1410722391314661478/Illustration92.png?ex=68b20d1c&is=68b0bb9c&hm=7fc9260fac702820e7ba3365817343de2596c5fc1c5d1680adb95e5c95496e1a&'
-    ,'https://images.squarespace-cdn.com/content/v1/5a88864349fc2b8e071336f8/1734388131810-MLJQJFSP0SQ3H09E6YPC/portfolio_update_2024_vox_1.jpg?format=1500w'
-    ,'https://images.squarespace-cdn.com/content/v1/5a88864349fc2b8e071336f8/1730330060423-RU7GH5U7NK71ERNUAOBT/2023portfolio_bb_1.jpg?format=1500w',]
+    'https://cdn.discordapp.com/attachments/762066841920995378/1412804866753167421/image.png?ex=68b9a090&is=68b84f10&hm=5ae384da629d7feccf72aef8224c37f1eadac35fd24fb3aaa541d825db137e79&',
+    'https://cdn.discordapp.com/attachments/762066841920995378/1412804923439186083/image.png?ex=68b9a09e&is=68b84f1e&hm=f73787eadce164267cd7d6f699b875f4b33c6b44a3f3d7bfd3f344aa1ad4a192&', 
+    'https://cdn.discordapp.com/attachments/762066841920995378/1412804979520966686/image.png?ex=68b9a0ab&is=68b84f2b&hm=2100bf429f80f479a54dc1fa832585c038995ce329d4490e03bac755d64b1b3e&', 
+    'https://cdn.discordapp.com/attachments/762066841920995378/1412805113436704848/image.png?ex=68b9a0cb&is=68b84f4b&hm=a0bcd73b3ecb5c6f424e8a562686c02656d70f82b6ed75932d4adec279e805bc&',
+]
 
 function Home() {
 
